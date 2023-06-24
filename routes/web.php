5595c3e57079fae7bc9b1073113396f74cdd8f72
anis096-dev <anis.awadi96@gmail.com>
2023-06-25 00:44:22 +0100

    2023_1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Livewire\Frontpage;
+use App\Http\Livewire\FrontPage;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
